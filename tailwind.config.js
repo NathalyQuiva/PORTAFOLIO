5e4989bf366924b1db3e7a057f5d49e59236e858
NathalyQuiva <nathalyquiva2@gmail.com>
2023-08-16 07:04:12 -0500

    1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./distr/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
