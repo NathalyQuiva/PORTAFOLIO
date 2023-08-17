2193a15d8279c5bc83fb3e2a8e4293b2b809e139
NathalyQuiva <nathalyquiva2@gmail.com>
2023-08-17 00:40:08 -0500

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme= require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./distr/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors:{
         primary:'#0f172a',
       },
+      fontFamily:{
+        roboto:['Roboto',... defaultTheme.fontFamily.sans]
+      }
     },
   },
   plugins: [],
